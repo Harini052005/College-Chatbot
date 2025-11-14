1d7eb52aa7d943a811f935345b681e825265b5b5
Harini Kannan <162604794+Harini052005@users.noreply.github.com>
2025-11-14 13:17:38 +0530

    Update config.py

--- harini/config.py
+++ harini/config.py
@@ -1,8 +1,9 @@
 import os
 
 # Secret key for Flask session management (Use a secure key in production)
-SECRET_KEY = os.getenv("SECRET_KEY", "4f3b2e9a7c8d6e1f5a0b3d2c9e4f8a7b")
+SECRET_KEY = os.getenv("SECRET_KEY", "place_secretkey_ here")
 
 
 # MongoDB Connection URI 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/chatbot_db")
+MONGO_URI = os.getenv("MONGO_URI", "place_mongodb_connection_here")
+
