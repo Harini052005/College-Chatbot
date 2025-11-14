8616fb33e93992170ec49b333cb21debc0a8cb99
Harini Kannan <162604794+Harini052005@users.noreply.github.com>
2025-11-14 13:18:20 +0530

    Update ex.py

--- harini/ex.py
+++ harini/ex.py
@@ -1,10 +1,11 @@
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
-password = "admin123"  
-hashed_password = "$2b$12$mZfiAkcsaudyFtbqqFLNjO/Cdb/zwQ.hHgXuQ.OgmNfLMRB26FWt6"  # Password from MongoDB
+password = "enter_password"  
+hashed_password = "paste_hashed_code_here"  # Password from MongoDB
 
 if bcrypt.check_password_hash(hashed_password, password):
     print("Password is correct!")
 else:
     print("Password is incorrect!")
+
